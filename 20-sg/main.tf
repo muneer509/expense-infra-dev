ab5635c14a1bca3792144bf4416b9d719029cda5
Muneer Shaik <mshaik4@berkadia.com>
2025-08-01 11:20:17 +0530

    practice

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -32,6 +32,17 @@ module "frontend_sg" {
     component = "expense"
   } 
 }
+module "bastion_sg" {
+  source = "git::https://github.com/daws-81s/terraform-aws-security-group.git?ref=main"
+  project_name = var.project_name
+  environment = var.environment   
+  sg_name = "bastion_sg"
+  vpc_id = local.vpc_id   
+  common_tags = {
+    terraform = "true"
+    component = "expense"
+  } 
+}
 resource "aws_security_group_rule" "mysql_to_backend" {
   type = "ingress"
   from_port = 3306
@@ -42,6 +53,14 @@ resource "aws_security_group_rule" "mysql_to_backend" {
   description = "Allow MySQL traffic from Backend"  
 
 }
+resource "aws_security_group_rule" "bastion_to_mysql" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  source_security_group_id = module.bastion_sg.id
+  security_group_id = module.mysql_sg.id
+}
 resource "aws_security_group_rule" "backend_to_frontend" {
   type = "ingress"
   from_port = 8080
@@ -49,6 +68,28 @@ resource "aws_security_group_rule" "backend_to_frontend" {
   protocol = "tcp"
   source_security_group_id = module.frontend_sg.id
   security_group_id = module.backend_sg.id
-
-
+}
+resource "aws_security_group_rule" "bastion_to_backend" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  security_group_id = module.backend_sg.id
+  source_security_group_id = module.bastion_sg.id
+}
+resource "aws_security_group_rule" "frontend_to_internet" {
+  type = "ingress"
+  from_port = 0
+  to_port = 0
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.frontend_sg.id
+}
+resource "aws_security_group_rule" "bastion_to_frontend" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  security_group_id = module.frontend_sg.id
+  source_security_group_id = module.bastion_sg.id
 }
